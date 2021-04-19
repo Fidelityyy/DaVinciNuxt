10ab7e738ad59e84ce6968bf946db475c7f67e47
Fidel <fidel.tonak@stud.hs-hannover.de>
2021-04-19 09:11:57 +0200

    Logo eingefügt und Grid bearbeitet

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,13 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="dark" id="navigation">
-    <b-navbar-brand href="/">Navbar</b-navbar-brand>
+  <b-navbar toggleable="lg" type="light" id="navigation">
+    <b-navbar-brand id ="logo" href="/">DaVinci</b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Test</b-nav-item>
+        <b-nav-item href="#">Historie</b-nav-item>
+        <b-nav-item href="#">Kontakt</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown text="Sprache" right>
-          <b-dropdown-item href="#">Deutsch</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -22,5 +20,18 @@ export default {};
 <style>
 #navigation {
   height: 100%;
+  background-color:rgb(250, 241, 228);
+}
+
+#logo {
+    font-family: 'Great Vibes', cursive;
+    font-size: 5vh;
+    color:black;
+}
+
+#logo:hover {
+      font-family: 'Great Vibes', cursive;
+    font-size: 5vh;
+    color:darkred;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <div id="header">
       <Navbar></Navbar>
     </div>
-    <div id="body"></div>
+    <div id="body">
+      <h1 id="logomain"><span style = "color: DarkRed">Da</span>Vinci</h1>
+    </div>
     <div id="footer"></div>
   </div>
 </template>
@@ -23,7 +25,7 @@ export default {
   margin: 0;
   padding: 0;
   grid-template-columns: 20vw 20vw 20vw 20vw 20vw;
-  grid-template-rows: 12.5vh 12.5vh 12.5vh 12.5vh 12.5vh 12.5vh 12.5vh 12.5vh;
+  grid-template-rows: 10vh 10vh 10vh 10vh 10vh 10vh 10vh 10vh 10vh 10vh;
   grid-template-areas:
     "header header header header header"
     "body body body body body"
@@ -32,7 +34,13 @@ export default {
     "body body body body body"
     "body body body body body"
     "body body body body body"
+    "body body body body body"
+    "body body body body body"
     "footer footer footer footer footer";
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitruvian_Man_by_Leonardo_da_Vinci.jpg");
+  background-repeat: repeat-y;
+  background-size: 100%;
+  background-color: antiquewhite;
 }
 
 #header {
@@ -46,8 +54,14 @@ export default {
   align-self: center;
 }
 
+#logomain {
+  font-family: "Great Vibes", cursive;
+  font-size: 20vh;
+}
+
 #footer {
   background-color: aqua;
   width: 100vw;
+  background-color: antiquewhite;
 }
 </style>
