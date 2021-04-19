273350554079104649f1f877fc8e33d67b6a5093
Fidel <fidel.tonak@stud.hs-hannover.de>
2021-04-19 08:29:35 +0200

    Navbar bearbeitet und index angepasst

---
+++ components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark" id="navigation">
+    <b-navbar-brand href="/">Navbar</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Test</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Sprache" right>
+          <b-dropdown-item href="#">Deutsch</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#navigation {
+  height: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,51 @@
 <template>
- 
+  <div id="container">
+    <div id="header">
+      <Navbar></Navbar>
+    </div>
+    <div id="body">
+    </div>
+    <div id="footer"></div>
+  </div>
 </template>
 
 <script>
-export default {}
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
 </script>
 
 <style>
+#container {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  grid-template-columns: 20vw 20vw 20vw 20vw 20vw;
+  grid-template-rows: 20vh 20vh 20vh 20vh 20vh;
+  grid-template-areas:
+    "header header header header header"
+    "body body body body body"
+    "body body body body body"
+    "body body body body body"
+    "footer footer footer footer footer";
+}
 
+#header {
+  grid-area: header;
+}
+
+#body {
+  display: flex;
+  grid-area: body;
+  justify-content: center;
+  align-self: center;
+}
+
+#footer {
+    background-color: aqua;
+    width: 100vw;
+}
 </style>
